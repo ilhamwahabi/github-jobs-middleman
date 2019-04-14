7729795e90e236465261d9e0877ef72adebdab08
iwgx <wahabiputra@gmail.com>
2019-04-14 14:54:05 +0700

    Able to fetch all jobs or based on query params

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send({ status: 'worked' })
+const baseUrl = "https://jobs.github.com"
+
+app.get('/jobs', async (request, response) => {
+  try {
+    let accQuery = ""
+    for (let query in request.query) {
+      accQuery += `&${query}=${request.query[query]}`
+    }
+
+    let requestUrl = `${baseUrl}/positions.json`
+    if (accQuery) requestUrl += `?${accQuery.slice(1)}`
+
+    const axiosResponse = await axios.get(requestUrl)
+    response.send(axiosResponse.data)
+  } catch (error) {
+    response.status(400).send({
+      message: 'error occurred'
+    })
+  }
 })
 
 app.listen(3000, () => {
