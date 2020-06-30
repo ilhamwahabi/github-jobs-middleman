96861b9d2e5333b162d165baf677fbdd7005e601
iwgx <wahabiputra@gmail.com>
2020-06-30 13:43:44 +0700

    Add new endpoint to get detail

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-const express = require('express')
-const cors = require('cors')
-const axios = require('axios')
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-const baseUrl = "https://jobs.github.com"
-const PORT = process.env.port || 3000
+const baseUrl = "https://jobs.github.com";
+const PORT = process.env.port || 3000;
 
-app.get('/jobs', async (request, response) => {
+app.get("/jobs", async (request, response) => {
   try {
-    let accQuery = ""
+    let accQuery = "";
     for (let query in request.query) {
-      accQuery += `&${query}=${request.query[query]}`
+      accQuery += `&${query}=${request.query[query]}`;
     }
 
-    let requestUrl = `${baseUrl}/positions.json`
-    if (accQuery) requestUrl += `?${accQuery.slice(1)}`
+    let requestUrl = `${baseUrl}/positions.json`;
+    if (accQuery) requestUrl += `?${accQuery.slice(1)}`;
 
-    const axiosResponse = await axios.get(requestUrl)
-    response.send(axiosResponse.data)
+    const axiosResponse = await axios.get(requestUrl);
+    response.send(axiosResponse.data);
   } catch (error) {
     response.status(400).send({
-      message: 'error occurred'
-    })
+      message: "error occurred",
+    });
   }
-})
+});
+
+app.get("/jobs/:id", async (request, response) => {
+  try {
+    const axiosResponse = await axios.get(
+      `${baseUrl}/positions/${request.params.id}.json`
+    );
+    response.send(axiosResponse.data);
+  } catch (error) {
+    response.status(400).send({
+      message: "error occurred",
+    });
+  }
+});
 
 app.listen(PORT, () => {
-  console.log('Server is running')
-})
+  console.log("Server is running");
+});
