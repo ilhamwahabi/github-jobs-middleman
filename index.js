6ac28711f7ec2157b7a92afb244cf679e150b6ed
iwgx <wahabiputra@gmail.com>
2020-07-01 06:11:19 +0700

    Enable /jobs/:id to get query params

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const axios = require("axios");
 const app = express();
 app.use(cors());
 
-const baseUrl = "https://jobs.github.com";
+const BASE_URL = "https://jobs.github.com";
 const PORT = process.env.port || 3000;
+
+function getQuery(query) {
+  let allQuery = "";
+  for (let item in query) {
+    allQuery += `&${item}=${query[item]}`;
+  }
+  return allQuery;
+}
 
 app.get("/jobs", async (request, response) => {
   try {
-    let accQuery = "";
-    for (let query in request.query) {
-      accQuery += `&${query}=${request.query[query]}`;
-    }
+    const query = getQuery(request.query);
 
-    let requestUrl = `${baseUrl}/positions.json`;
-    if (accQuery) requestUrl += `?${accQuery.slice(1)}`;
+    let requestUrl = `${BASE_URL}/positions.json`;
+    if (query) requestUrl += `?${query.slice(1)}`;
 
     const axiosResponse = await axios.get(requestUrl);
     response.send(axiosResponse.data);
@@ -29,9 +34,12 @@ app.get("/jobs", async (request, response) => {
 
 app.get("/jobs/:id", async (request, response) => {
   try {
-    const axiosResponse = await axios.get(
-      `${baseUrl}/positions/${request.params.id}.json`
-    );
+    const query = getQuery(request.query);
+
+    let requestUrl = `${BASE_URL}/positions/${request.params.id}.json`;
+    if (query) requestUrl += `?${query.slice(1)}`;
+
+    const axiosResponse = await axios.get(requestUrl);
     response.send(axiosResponse.data);
   } catch (error) {
     response.status(400).send({
